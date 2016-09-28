8c175fb2aed3ebc491584807d54a351dd977d084
Pausali Sanyal <pasanyal@gmail.com>
2016-09-28 20:24:55 +1000

    Added comments over functions.

--- app/stores/progressBarsStore.js
+++ app/stores/progressBarsStore.js
@@ -2,23 +2,28 @@ var dispatcher = require("../dispatcher");
 
 function ProgressBarStore() {
     var listeners = [];
+    
+    /* Bars to be displayed in the html */
     var bars = [{ name: "pb1", width: 0, className: "inner-progress-barB", displayWidth: 0 }, 
                 { name: "pb2", width: 0, className: "inner-progress-barB", displayWidth: 0 }];
-                    
+    
     function getBars() {
         return bars;
     }
-
+    
+    /* Function to be called on change of bar */
     function onChange(listener) {
         listeners.push(listener);       
     }
     
+    /* Triggers the specific listener */
     function triggerListeners() {                                    
         listeners.forEach(function (listener) {
             listener(bars);
         });
     }
 	
+    /* Common function to change the width of the bars */
 	function setProgressBarWidth(progressBar, widthChangeSize) {
         progressBar.displayWidth += parseInt(widthChangeSize);
         if(progressBar.displayWidth < 0)
@@ -38,6 +43,7 @@ function ProgressBarStore() {
         }
     }
     
+    /* Registers function for the specific payload type */
     dispatcher.register(function (payload) {
         var payloadType = payload.type;
         
